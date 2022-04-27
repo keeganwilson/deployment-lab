6bae5a0cef4add4a0b863d9ff49118c2a0835771
Keegan Wilson <keegan.wilson@gmail.com>
2022-04-27 14:59:32 -0600

    fixed index.js

--- server/index.js
+++ server/index.js
@@ -3,8 +3,12 @@ const path = require('path')
 
 const app = express();
 
-app.get('/js', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/main.js'))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'))
 })
 
-app.use('/js', express.static(path.join(__dirname, 'public/main.js')))
+app.use('/js', express.static(path.join(__dirname, '../main.js')))
+
+const port = process.env.PORT || 4005
+
+app.listen(port, () => {console.log(`Listening on port ${port}`)})
