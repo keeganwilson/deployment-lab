7532856c088dcc5941aa0859fb75aa04a908a689
Keegan Wilson <keegan.wilson@gmail.com>
2022-04-27 16:07:55 -0600

    silly css

--- server/index.js
+++ server/index.js
@@ -4,10 +4,10 @@ const path = require('path')
 const app = express();
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html'))
+    res.sendFile(path.join(__dirname, '../styles/index.html'))
 })
 
-app.use('/js', express.static(path.join(__dirname, '../main.js')))
+app.use(express.static("styles"))
 
 const port = process.env.PORT || 4005
 
